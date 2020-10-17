2a65d69fcff042fa5cc1a439c28475bb16272b1a
Soledad Merino <soledadmerino.1994@gmail.com>
2020-10-17 19:08:36 -0300

    Hasta clase 3

--- src/app/clothes-list/clothes-list.component.ts
+++ src/app/clothes-list/clothes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Prenda } from './Prenda';
 
 @Component({
   selector: 'app-clothes-list',
@@ -7,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClothesListComponent implements OnInit {
 
-  clothes = {
-    "name": "remera",
-    "size": "2",
-    "price": 500,
-    "stock": 4,
-    "image": "assets/img/remera.jpeg"
-  }
-
+  prendas: Prenda[] = [
+    {
+      name: "Buzo",
+      size: "12",
+      price: 850,
+      stock: 1,
+      image: "assets/img/buzo.jpeg",
+    },
+    {
+      name: "Vestido Sandias",
+      size: "12 a 18 m",
+      price: 740,
+      stock: 1,
+      image: "assets/img/vestido-sandia.jpg",
+    },
+    {
+      name: "Body autitos",
+      size: "3 a 6 m",
+      price: 490,
+      stock: 1,
+      image: "assets/img/body-autitos.jpg",
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/clothes-list/prenda.ts
@@ -0,0 +1,7 @@
+export interface Prenda {
+    name: string;
+    size: string;
+    price: number;
+    stock: number;
+    image: string;
+}
